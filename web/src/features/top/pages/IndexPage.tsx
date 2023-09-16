168f6f473ccca1fdee5986116c8fdfd536c2b189
atsumarukun <atys.smz@gmail.com>
2023-09-17 02:29:23 +0900

    Show time in index page.

---
+++ web/src/features/top/pages/IndexPage.tsx
@@ -0,0 +1,30 @@
+import { Text, VStack } from "@chakra-ui/react";
+import moment from "moment-timezone";
+import { useState } from "react";
+
+export function IndexPage() {
+  moment.updateLocale("ja", {
+    weekdays: [
+      "日曜日",
+      "月曜日",
+      "火曜日",
+      "水曜日",
+      "木曜日",
+      "金曜日",
+      "土曜日",
+    ],
+    weekdaysShort: ["日", "月", "火", "水", "木", "金", "土"],
+  });
+  const [time, setTime] = useState(moment().tz("Asia/Tokyo"));
+
+  setInterval(() => {
+    setTime(moment().tz("Asia/Tokyo"));
+  }, 100);
+
+  return (
+    <VStack mt="20vh" spacing={0}>
+      <Text fontSize="7xl">{time.format("h:mm")}</Text>
+      <Text fontSize="xl">{time.format("MM月DD日 dddd")}</Text>
+    </VStack>
+  );
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { IndexPage } from "@/features/top/pages/IndexPage";
+
 export default function Home() {
-  return <></>;
+  return <IndexPage />;
 }
