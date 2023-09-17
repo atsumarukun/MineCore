b258ebf610145791a2f8fca8eab86600689f0c76
atsumarukun <atys.smz@gmail.com>
2023-09-18 00:39:11 +0900

    Change time format.

--- web/src/features/top/pages/IndexPage.tsx
+++ web/src/features/top/pages/IndexPage.tsx
@@ -15,23 +15,21 @@ export function IndexPage() {
     ],
     weekdaysShort: ["日", "月", "火", "水", "木", "金", "土"],
   });
-  const [time, setTime] = useState(moment().tz("Asia/Tokyo").format("HH:mm"));
-  const [date, setDate] = useState(
-    moment().tz("Asia/Tokyo").format("MM月DD日 dddd")
-  );
+  const [time, setTime] = useState(moment().tz("Asia/Tokyo"));
 
   useEffect(() => {
     const id = setInterval(() => {
-      setTime(moment().tz("Asia/Tokyo").format("HH:mm"));
-      setDate(moment().tz("Asia/Tokyo").format("MM月DD日 dddd"));
+      setTime(moment().tz("Asia/Tokyo"));
     }, 1000);
     return () => clearInterval(id);
   }, [time]);
 
   return (
     <VStack mt="20vh" spacing={0}>
-      <Text fontSize={{ base: "5xl", md: "7xl" }}>{time}</Text>
-      <Text fontSize={{ base: "lg", md: "xl" }}>{date}</Text>
+      <Text fontSize={{ base: "5xl", md: "7xl" }}>{time.format("hh:mm")}</Text>
+      <Text fontSize={{ base: "lg", md: "xl" }}>
+        {time.format("MM月DD日 dddd")}
+      </Text>
     </VStack>
   );
 }
