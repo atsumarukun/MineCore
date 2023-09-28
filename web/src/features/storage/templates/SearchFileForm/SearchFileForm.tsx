56fcc663abb96bd3b76b1ea593f618254e24fed0
atsumarukun <atys.smz@gmail.com>
2023-09-28 21:28:18 +0900

    Change searchbar in storage.

--- web/src/features/storage/templates/SearchFileForm/SearchFileForm.tsx
+++ web/src/features/storage/templates/SearchFileForm/SearchFileForm.tsx
@@ -5,18 +5,46 @@ import {
   InputGroup,
   InputLeftElement,
 } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { BiSearch } from "react-icons/bi";
 import { SearchFileFormSchema, searchFileFormSchema } from "./schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export function SearchFileForm() {
-  const { register } = useForm<SearchFileFormSchema>({
+  const router = useRouter();
+  const { register, setValue, handleSubmit } = useForm<SearchFileFormSchema>({
     resolver: zodResolver(searchFileFormSchema),
   });
 
+  const onSubmit: SubmitHandler<SearchFileFormSchema> = (data) => {
+    if (data.name) {
+      router.push({
+        query: {
+          ...router.query,
+          name: data.name,
+        },
+      });
+    } else {
+      delete router.query.name;
+      router.push({
+        query: {
+          ...router.query,
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    setValue(
+      "name",
+      typeof router.query.name === "string" ? router.query.name : ""
+    );
+  }, [router.query]);
+
   return (
-    <form style={{ width: "100%" }}>
+    <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
       <HStack w="100%" spacing={0}>
         <InputGroup>
           <InputLeftElement pl={4} color="whiteAlpha.300">
@@ -27,6 +55,7 @@ export function SearchFileForm() {
             pl={12}
             bgColor="blackAlpha.500"
             border="unset"
+            autoComplete="off"
             {...register("name")}
           />
         </InputGroup>
